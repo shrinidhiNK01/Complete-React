8bc2d651e2f57a25b4b5fccf5dd2f823fdb2b905
shrinidhiNK01 <kulkarnishrinidhi0001@gmail.com>
2026-05-25 23:04:59 +0530

    add all

---
+++ first/src/App.jsx
@@ -0,0 +1,11 @@
+import First from "./First"
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl bg-red-500">Hello React</h1>
+      <First />
+    </div>
+  );
+}
+
+export default App;
---
+++ first/src/First.jsx
@@ -0,0 +1,10 @@
+function First() {
+    return (
+        <h1>Hi I am Shrinidhi , I am Writing a Story Called Lost States and it is an amazing story
+            with the concepts of quantum physics and string theory.
+            The main character is a young physicist who discovers a way to travel between parallel universes.
+        </h1>
+    )
+}
+
+export default First;
